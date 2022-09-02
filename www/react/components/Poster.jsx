e0142dc47314f891e10a45ad4b7802d7521ad653
Anders Pedersen <hot415@hotmail.com>
2022-09-02 22:33:48 +0200

    Added support for marking on a poster via CSS class

--- www/react/components/Poster.jsx
+++ www/react/components/Poster.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default class Poster extends React.Component {
     render() {
+        const imageClassNames = ["image", this.props?.marked ? "marked" : null].filter(v => v).join(" ");
         return <div className="card">
-            <div className="image" style={{backgroundImage: `url('${this.props.image}')`}}></div>
+            <div className={imageClassNames} style={{backgroundImage: `url('${this.props.image}')`}}></div>
             <div className="title">{this.props.title}</div>
             <div className="title secondary">{this.props.api}</div>
         </div>;
