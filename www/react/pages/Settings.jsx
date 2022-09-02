2cd628c14865bd8df48461e04d30c56f5895669e
Anders Pedersen <hot415@hotmail.com>
2022-09-02 22:39:40 +0200

    Change toast test code to cycle the available toast types

--- www/react/pages/Settings.jsx
+++ www/react/pages/Settings.jsx
@@ -3,7 +3,7 @@ import ToastContainer from "../components/ToastContainer";
 
 export default class Settings extends React.Component {
     render() {
-        ToastContainer.add("#"+ToastContainer.getAutoIncrement());
+        ToastContainer.add("#"+ToastContainer.getAutoIncrement(), ['success', 'info', 'warning', 'error'][ToastContainer.getAutoIncrement() % 4]);
         return "NOTHING HERE YET.";
     }
 }
